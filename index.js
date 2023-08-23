ed84f01d7265130a3ba44dcd499b7292797e3215
JayeshDarji25 <142973291+JayeshDarji25@users.noreply.github.com>
2023-08-23 16:37:25 +0530

    commit first

---
+++ index.js
@@ -0,0 +1,35 @@
+const name = document.getElementById('name');
+const comment = document.getElementById('comment');
+const male = document.getElementById('male');
+var radios = document.getElementsByName("gen");
+var formValid = false;
+
+function checker() {
+    if(!name.value.trim().length){
+        alert("All fields are compulsory");
+        name.focus();
+        return false;
+    }
+    else if(!comment.value.trim().length){
+        alert("All fields are compulsory");
+        comment.focus();
+        return false;
+    }
+    else{
+        var i = 0;
+        while (!formValid && i < radios.length) {
+            if (radios[i].checked) {
+                formValid = true;
+                break;
+            }
+            i++;        
+        }
+
+        if (!formValid){
+            alert("All fields are compulsory");
+            male.focus();
+            return false;   
+        } 
+    }
+    return true;
+}
